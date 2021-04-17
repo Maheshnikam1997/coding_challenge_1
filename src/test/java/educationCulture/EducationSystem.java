98881a092d69333853315785c817183267f72057
Maheshnikam1997 <sam.nikam14@gmail.com>
2021-04-17 20:39:58 +0530

    comments added before calling methods of each class

--- src/test/java/educationCulture/EducationSystem.java
+++ src/test/java/educationCulture/EducationSystem.java
@@ -7,15 +7,18 @@ public class EducationSystem {
 		Engineering eg = new Engineering();
 		ComputerScienceENgineering cse = new ComputerScienceENgineering();
 		
+		//all methods define in graduation class
 		System.out.println(" all methods define in graduation class:\n");
 		gd.streams();
 		gd.hscPercentage();
 		gd.ageCritera();
 		
+		//all methods define in Engineering class
 		System.out.println("\n all methods define in Engineering class:\n");
 		eg.streams();
 		eg.typeOfEngineering();
 		
+		//all methods define in ComputerScienceEngineering class
 		System.out.println("\n all methods define in ComputerScienceEngineering class:\n");
 		cse.hscPercentage();
 		cse.subjects();

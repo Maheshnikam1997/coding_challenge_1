41d9a615d9d24b9a8d41c0aae59b20b36750a099
Maheshnikam1997 <sam.nikam14@gmail.com>
2021-04-17 21:25:43 +0530

    result added in main file

--- src/test/java/educationCulture/EducationSystem.java
+++ src/test/java/educationCulture/EducationSystem.java
@@ -6,7 +6,6 @@ public class EducationSystem {
 		Graduation gd = new Graduation();
 		Engineering eg = new Engineering();
 		ComputerScienceENgineering cse = new ComputerScienceENgineering();
-		
 		//all methods define in graduation class
 		System.out.println(" all methods define in graduation class:\n");
 		gd.streams();
@@ -24,3 +23,23 @@ public class EducationSystem {
 		cse.subjects();
 	}
 }
+
+
+/**
+ * all methods define in graduation class:
+
+BCA, BBA, B.E.
+for graduation minimun 60% is critera
+for graduation minimun age critera for cadidate is 20 years
+
+ all methods define in Engineering class:
+
+Computer Sciance, Information Technology, Electronics & Mechanical Engineering
+Engineeing can be done as Regular as well as from distance learing
+
+ all methods define in ComputerScienceEngineering class:
+
+To get admission for Computer Science Engineering HSC percentage should be minimun 65%
+Core Java, C sharp & Python are subjects offered in Computer Science Engineering
+/
+ */
